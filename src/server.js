f1158a81ee50ad921e4bfb019b6cf3639b915404
min199973 <107176389+min199973@users.noreply.github.com>
2023-02-16 01:56:03 +0900

    Youtube #3.5 ~ #3.10

--- src/server.js
+++ src/server.js
@@ -4,15 +4,17 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = (req, res) => {
-  return res.send("<h1>I still love you</h1>");
-};
-const handleLogin = (req, res) => {
-  return res.send({ message: "Login here" });
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
 };
 
+const handleHome = (req, res) => {
+  return res.send("<h1>Hello</h1>");
+};
+
+app.use(logger);
 app.get("/", handleHome);
-app.get("/login", handleLogin);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
